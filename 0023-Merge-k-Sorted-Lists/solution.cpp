243049264c7bf4dbec66060c1bb534bc9323bf51
rsr-systems <156883423+rsr-systems@users.noreply.github.com>
2026-09-17 10:26:07 +0530

    Implement mergeKLists for merging k sorted lists

---
+++ 0023-Merge-k-Sorted-Lists/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+
+        for (auto list : lists) {
+            if (list) pq.push(list);
+        }
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (!pq.empty()) {
+            ListNode* node = pq.top();
+            pq.pop();
+
+            tail->next = node;
+            tail = tail->next;
+
+            if (node->next) {
+                pq.push(node->next);
+            }
+        }
+
+        return dummy.next;
+    }
+};
