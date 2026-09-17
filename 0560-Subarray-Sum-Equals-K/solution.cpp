4a8657704e7dcadad2ac3c56ae662f8fdc3f780a
rsr-systems <156883423+rsr-systems@users.noreply.github.com>
2026-09-17 09:41:28 +0530

    Implement subarray sum equals K solution

---
+++ 0560-Subarray-Sum-Equals-K/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> prefix_counts;
+        prefix_counts[0] = 1;
+        
+        int current_sum = 0;
+        int count = 0;
+        
+        for (int num : nums) {
+            current_sum += num;
+            
+            if (prefix_counts.find(current_sum - k) != prefix_counts.end()) {
+                count += prefix_counts[current_sum - k];
+            }
+            
+            prefix_counts[current_sum]++;
+        }
+        
+        return count;
+    }
+};
